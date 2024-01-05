de5ff66736c2effd7a2a6afb8af7b6cbf5b09b62
edgarcsi <edgar.munoz@gpocsi.mx>
2024-01-05 14:18:33 -0800

    creacion de componentes/rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    //dejar la ruta basia para que inicie.. 
+    path: '',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+     
+  },
+ 
+];
+
+@NgModule({
+  declarations: [],
+  imports: [ 
+    [RouterModule.forRoot(routes)],
+  ],
+  exports: [RouterModule] //exporta todos los modulos agregados
+})
+export class AppRoutingModule { }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { UserListComponent } from './user-list/user-list.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserListComponent, NewUserComponent],
   imports: [
     CommonModule,
     UserRoutingModule
